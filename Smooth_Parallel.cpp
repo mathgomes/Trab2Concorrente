67c8eaecf0876285acdf88f38661c46633f3cf42
renatojose <renato.jose93@gmail.com>
2015-11-15 22:38:04 -0200

    Update and rename openmpi.cpp to Smooth_Parallel.cpp

--- Smooth_Parallel.cpp
+++ Smooth_Parallel.cpp
@@ -4,7 +4,6 @@
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-//#include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/features2d/features2d.hpp"
 #include <omp.h>
@@ -16,7 +15,7 @@ using namespace std;
 using namespace cv;
 
 #define SQUARE_AREA 25
-//o processador é um quad core
+//o processador é um quad core.
 #define NUMTHREADS 4
 
 Mat src, dst[3], dst2[3], finImg, aux, out;
@@ -27,41 +26,44 @@ int display_finImg(int delay, String&, Mat&);
 
 int main( int argc, char** argv) {
 
-    /// General use variables
     int rank, top = 2, bottom = 2, left = 2, right = 2, borderType = BORDER_CONSTANT, tamLin, tamCol, bloco, resto, numNos;
     String window_name = "Smoothing ";
 
     MPI_Init(&argc, &argv);
     MPI_Status estado;
-    //descobre o rank do nó
+    
+    //rank do nó.
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    //descobre quantos nós a maquina suporta
+    
+    //quantidade de nós que a máquina pode processar.
     MPI_Comm_size(MPI_COMM_WORLD, &numNos);
-    //variavel auxiliar que conterá a dimensão de cada pedaço e enviará para nó
-    int dimensoes[3];
+    
+    //variavel auxiliar que conterá a dimensão de cada bloco da imagem e enviará para cada nó.
+    int tamanhoBloco[3];
 
+    //condição para entrar no nó master.
     if(rank == 0){
 
-	    ///Values that will be inserted in image border( solves border pixels problem )
+	    //Valores que serão inseridos na borda da imagem.
 	    Scalar value( 0, 0, 0);
 
-	    /// Load the source image
+	    //Carrega a imagem ser usada.
 	    int imgType = atoi(argv[2]);
 	    src = imread( argv[1], imgType );
-
+	     
+	    //Cria a borda na imagem.
 	    copyMakeBorder(src,src, top, bottom,left, right, borderType, value);
+	  
+	    //clona a imagem de entrada.
 	    finImg = src.clone();
-	    
-	    
 
-	    //copyMakeBorder( src, src, top, bottom, left, right, borderType, value );
-	    
-	    //divide a img em partes iguais para paralelizar no openmpi--------------------
+	    //divide a img em partes iguais para paralelizar no openmpi
 	    int divisaoLinha = tamLin/(numNos - 1);
             int divisaoColuna = tamCol;
 	    int restoDivisao = tamLin%(numNos - 1);
-            //-----------------------------------------------------------------------------
+
 	    int i;
+	    //paralelização dos blocos da imagem, enviando cada bloco para um nó diferente.
 	    for(i=0;i< numNos;++i){
 
 		//Neste "if else", recortamos um pedaço da img original que será salvo em 'aux' para depois enviar para os nós.
@@ -71,39 +73,40 @@ int main( int argc, char** argv) {
 			aux = src(Rect(0,divisaoLinha*(i-1), divisaoColuna, divisaoLinha + restoDivisao));		
 		}
 
-		dimensoes[0] = aux.cols;
-		dimensoes[1] = aux.rows;
-		dimensoes[2] = imgType;
+		tamanhoBloco[0] = aux.cols;
+		tamanhoBloco[1] = aux.rows;
+		tamanhoBloco[2] = imgType;
 
-		//envia as dimensões para o nó.
-		MPI_Send(dimensoes, 3, MPI_INT, i, 0, MPI_COMM_WORLD);
+		//envia as o tamanho do bloco da imagem para o nó.
+		MPI_Send(tamanhoBloco, 3, MPI_INT, i, 0, MPI_COMM_WORLD);
 		
 		//envia o pedaço da imagem para o nó, dependendo do tipo da imagem.
-		if(dimensoes[2] == 0){
-		   MPI_Send(aux.data, dimensoes[0]*dimensoes[1], MPI_UNSIGNED_CHAR, i, 1, MPI_COMM_WORLD);
-		}else if(dimensoes[2] == 1){
-		   MPI_Send(aux.data, dimensoes[0]*dimensoes[1]*3, MPI_UNSIGNED_CHAR, i, 1, MPI_COMM_WORLD);
+		if(tamanhoBloco[2] == 0){
+		   MPI_Send(aux.data, tamanhoBloco[0]*tamanhoBloco[1], MPI_UNSIGNED_CHAR, i, 1, MPI_COMM_WORLD);
+		}else if(tamanhoBloco[2] == 1){
+		   MPI_Send(aux.data, tamanhoBloco[0]*tamanhoBloco[1]*3, MPI_UNSIGNED_CHAR, i, 1, MPI_COMM_WORLD);
 		}
 
 	    }
 	
 	    //Recebe cada pedaço de cada nó.
 	    for(i=1; i<numNos; ++i){
-		MPI_Recv(dimensoes, 3, MPI_INT, i, 2, MPI_COMM_WORLD, &estado);
+		MPI_Recv(tamanhoBloco, 3, MPI_INT, i, 2, MPI_COMM_WORLD, &estado);
 		
-		if(dimensoes[2] == 0){
+		//Verifica se a imagem é Grayscale ou RGB.
+		if(tamanhoBloco[2] == 0){
 
-		   Mat pedaco(dimensoes[1], dimensoes[0] ,CV_8U);
-		   MPI_Recv(pedaco.data, dimensoes[0]*dimensoes[1], MPI_UNSIGNED_CHAR, i, 3, MPI_COMM_WORLD, &estado);
+		   Mat pedaco(tamanhoBloco[1], tamanhoBloco[0] ,CV_8U);
+		   MPI_Recv(pedaco.data, tamanhoBloco[0]*tamanhoBloco[1], MPI_UNSIGNED_CHAR, i, 3, MPI_COMM_WORLD, &estado);
 		   if(i == 1) {
 			vconcat(pedaco, out);
 		   } else {
 			vconcat(out, pedaco, out);
 		   }
 
-		}else if(dimensoes[2] == 1){
-		   Mat pedaco(dimensoes[1], dimensoes[0] ,CV_8U);
-		   MPI_Recv(pedaco.data, dimensoes[0]*dimensoes[1]*3, MPI_UNSIGNED_CHAR, i, 3, MPI_COMM_WORLD, &estado);
+		}else if(tamanhoBloco[2] == 1){
+		   Mat pedaco(tamanhoBloco[1], tamanhoBloco[0] ,CV_8U);
+		   MPI_Recv(pedaco.data, tamanhoBloco[0]*tamanhoBloco[1]*3, MPI_UNSIGNED_CHAR, i, 3, MPI_COMM_WORLD, &estado);
 		   if(i == 1) {
 			vconcat(pedaco, out);
 		   } else {
@@ -112,18 +115,17 @@ int main( int argc, char** argv) {
 		}
 	    }
 	    
+	    //salva a imagem no diretório.
  	    imwrite("novaImg.jpg", out);
     }else{	
 	    //dentro de cada nó ele executará esse else.
-	    MPI_Recv(dimensoes, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, &estado);
-
+	    MPI_Recv(tamanhoBloco, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, &estado);
 	
-	    /// grayScale image section
-	    if(dimensoes[2] == 0) {
+	    if(tamanhoBloco[2] == 0) {
 		
 		//cria um mat do tamanho do pedaco;	
-		Mat pedaco(dimensoes[1], dimensoes[0], CV_8U);	
-		MPI_Recv(pedaco.data, dimensoes[0]*dimensoes[1], MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD, &estado);
+		Mat pedaco(tamanhoBloco[1], tamanhoBloco[0], CV_8U);	
+		MPI_Recv(pedaco.data, tamanhoBloco[0]*tamanhoBloco[1], MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD, &estado);
 		
 		tamLin = pedaco.rows;
 	    	tamCol = pedaco.cols;
@@ -136,40 +138,36 @@ int main( int argc, char** argv) {
 		#pragma omp parallel for
 		for(int i=0; i < NUMTHREADS; ++i){
 		   if(i == NUMTHREADS-1)
-		      mediaBlur(bloco*i, (bloco*(i+1)) - 1);
+		      mediaBlurGrayscale(bloco*i, (bloco*(i+1)) - 1);
 		   else
-		      mediaBlur(bloco*i, (bloco*(i+1)) - 1 + resto);
+		      mediaBlurGrayscale(bloco*i, (bloco*(i+1)) - 1 + resto);
 		}
 
+                //Retira a borda criada anteriormente da imagem.
 		finImg = finImg.colRange(3, (finImg.cols-3));
 		finImg = finImg.rowRange(3, (finImg.rows-3));
+		
 		//libera espaço na memória
 		pedaco.release();
 
-		//if( display_finImg( DELAY_CAPTION,window_name,finImg) != 0 ) { return 0; }
-		//waitKey(0);
 	    } else {
 
-		    /// Split the image in channels
+		    //Separa a imagem original nos três canais disponíveis(RGB).
 		    split(src,dst);
 		    split(src, dst2);
 		    
-		    Mat pedaco(dimensoes[1], dimensoes[0], CV_8UC3);	
-		    MPI_Recv(pedaco.data, dimensoes[0]*dimensoes[1]*3, MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD, &estado);
+		    Mat pedaco(tamanhoBloco[1], tamanhoBloco[0], CV_8UC3);	
+		    MPI_Recv(pedaco.data, tamanhoBloco[0]*tamanhoBloco[1]*3, MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD, &estado);
 			
 		    tamLin = pedaco.rows;
 	    	    tamCol = pedaco.cols;
 
-       	            //divide a img em partes iguais para paralelizar no openmp
+       	            //divide a img em partes iguais para paralelizar no openmp.
 		    bloco = tamLin/NUMTHREADS;
 		    resto = tamLin%NUMTHREADS;
 
-		    /// Apply medianBlur in each channel
+		    //Aplica MediaBlur para cada bloco da img.
 		    for(int j=0;j<3;++j){
-
-			//insere a borda de tamanhos especificados
-			//copyMakeBorder( dst[j], dst[j], top, bottom, left, right, borderType, value );
-			//copyMakeBorder( dst2[j], dst2[j], top, bottom, left, right, borderType, value );
 
 			//percorrer o loop para cada bloco da imagem definida.
 			#pragma omp parallel for
@@ -186,35 +184,35 @@ int main( int argc, char** argv) {
 		    }
 
 
-		    /// Push the channels into the Mat vector
+		    //Coloca os canais RGB dentro da matriz rgb.
 		    vector<Mat> rgb;
 		    rgb.push_back(dst[0]); //blue
 		    rgb.push_back(dst[1]); //green
 		    rgb.push_back(dst[2]); //red
 
 
-		/// Merge the three channels
+		//Junta os três canais da imagem na imagem final.
 		merge(rgb, finImg);
-	   	//if( display_finImg( DELAY_CAPTION,window_name,finImg) != 0 ) { return 0; }
-	    	//waitKey(0);
 		pedaco.release();
 	    }
-
-	    dimensoes[0] = finImg.size().width;
-	    dimensoes[1] = finImg.size().height;
-	    MPI_Send(dimensoes, 3, MPI_INT, 0, 2, MPI_COMM_WORLD);
-	    if(dimensoes[2]==0){
-	    	MPI_Send(out.data, dimensoes[0]*dimensoes[1], MPI_UNSIGNED_CHAR, 0, 3, MPI_COMM_WORLD);
-            }else if(dimensoes[2]==1){
-		MPI_Send(out.data, dimensoes[0]*dimensoes[1]*3, MPI_UNSIGNED_CHAR, 0, 3, MPI_COMM_WORLD);
+		
+	    tamanhoBloco[0] = finImg.size().width;
+	    tamanhoBloco[1] = finImg.size().height;
+	    
+	    MPI_Send(tamanhoBloco, 3, MPI_INT, 0, 2, MPI_COMM_WORLD);
+	    //Envia a imagem final para o master.
+	    if(tamanhoBloco[2]==0){
+	    	MPI_Send(out.data, tamanhoBloco[0]*tamanhoBloco[1], MPI_UNSIGNED_CHAR, 0, 3, MPI_COMM_WORLD);
+            }else if(tamanhoBloco[2]==1){
+		MPI_Send(out.data, tamanhoBloco[0]*tamanhoBloco[1]*3, MPI_UNSIGNED_CHAR, 0, 3, MPI_COMM_WORLD);
 	    }
     }
-
+    //FInaliza o o OpenMPI.
     MPI_Finalize();
     return 0;
 }
 
-
+//Método de aplicação do mediaBlur para as imagens em RGB.
 void mediaBlurRGB(int inicio, int fim, int indice){
 
     int colSize = finImg.cols, sum, inicioLinha;
@@ -242,7 +240,9 @@ void mediaBlurRGB(int inicio, int fim, int indice){
     }
 }
 
-void mediaBlur(int inicio, int fim){
+
+//Método de aplicação do mediaBlur para as imagens em Grayscale.
+void mediaBlurGrayscale(int inicio, int fim){
     
     int colSize = finImg.cols, sum, inicioLinha;
     double media;
@@ -267,13 +267,4 @@ void mediaBlur(int inicio, int fim){
         finImg.at<uchar>(i,j) = media;
         }
     }
-}
-
-
-int display_finImg(int delay, String &window_name, Mat &finImg){
-
-    imshow( window_name, finImg);
-    int c = waitKey ( delay );
-    if( c >= 0 ) { return -1; }
-    return 0;
 }
